5a25dacaa67ac265841fee152fabe2a7b8d1a5ff
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-14 09:52:01 +0300

    we

--- project.py
+++ project.py
@@ -1,23 +1,30 @@
 from sharepoint import SharePoint
 from openpyxl import Workbook
+import streamlit as st
+import pandas as pd
 
 # get clients sharepoint list
-clients = SharePoint().connect_to_list(ls_name='Clients')
+clients = SharePoint().connect_to_list(ls_name='Maintenance Report')
+
+# create DataFrame from clients list
+df = pd.DataFrame(clients)
+
+st.write(df)
 
 # create excel workbook
 wb = Workbook()
 
-dest_filepath = 'client_list.xlsx'
+dest_filepath = 'maintenance_list.xlsx'
 
 # create worksheet
 ws = wb.active
-ws.title = 'Client List'
+ws.title = 'Maintenance'
 
 # setting sharepoint list values to excel cells
 for idx, client in enumerate(clients, 1):
     ws.cell(column=1, row=idx, value=client['Title'])
-    ws.cell(column=2, row=idx, value=client['AddressInfo: Street'])
-    ws.cell(column=3, row=idx, value=client['AddressInfo: City'])
+    ws.cell(column=2, row=idx, value=client['Details'])
+    ws.cell(column=3, row=idx, value=client['ID'])
 
 # save workbook
 wb.save(filename=dest_filepath)
